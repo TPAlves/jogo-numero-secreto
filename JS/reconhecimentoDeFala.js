e79a7a8482bca8465d6aed4e50194b516660b8b7
Thiago Pereira Alves <74566269+TPAlves@users.noreply.github.com>
2023-10-25 19:24:54 -0300

    update

--- JS/reconhecimentoDeFala.js
+++ JS/reconhecimentoDeFala.js
@@ -10,12 +10,30 @@ recognition.start();
 
 recognition.addEventListener('result', onSpeak);
 
+// function onSpeak(event) {
+//     chute = event.results[0][0].transcript;
+//     verificaGameOver(chute);
+//     exibeMensagem(chute);
+//     verificaValorValido(chute);
+    
+// }
+
 function onSpeak(event) {
     chute = event.results[0][0].transcript;
     verificaGameOver(chute);
     exibeMensagem(chute);
     verificaValorValido(chute);
-    
+
+    // Removendo todos os espaços 
+    chute = chute.replace(/\s/g, '');
+    chute = chute.trim();
+
+    // Validando se é um número
+    if (!isNaN(chute)) {
+        chute = parseInt(chute);
+    } else {
+        chute = null;
+    }
 }
 
 function exibeMensagem(chute) {
